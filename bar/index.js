d14a9950aa914f67aefd71045a9df6e362ed067b
Pamela Baez <pamelabaez182@gmail.com>
2023-11-10 18:03:39 +0100

    new changes

---
+++ bar/index.js
@@ -0,0 +1,7 @@
+let bebida;
+bebida = prompt ("Que bebida quieres?")
+let age;
+age = prompt ("Que edad tienes?")
+
+if (age => 18) {alert("Aqui tienes tu bebida!")}
+else if(age=> 14 && (drink==="coca cola" || drink=== "milk")){alert("Aqui tienes tu bebida")}
